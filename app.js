0d967be7c3864bf0eabc8dc45ac63ed5803833e7
lizhijuan <lizj@mchina.cn>
2019-08-26 15:38:32 +0800

    nodejs+koa+mysql cms后台管理服务端

---
+++ app.js
@@ -0,0 +1,17 @@
+require('module-alias/register')
+
+const Koa = require('koa')
+const parser = require('koa-bodyparser')
+const InitManager = require('./core/init')
+const catchError = require('./middlewares/exception')
+
+require('./app/models/classic')
+const app = new Koa()
+app.use(catchError)
+app.use(parser())
+
+console.log(process.cwd())
+InitManager.initCore(app)
+
+
+app.listen(3000)
